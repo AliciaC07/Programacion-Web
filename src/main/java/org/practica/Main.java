b4115c0e0894d048f8b9771fa2c5f50b453b8a07
Aliciac07 <20170465@ce.pucmm.edu.do>
2021-05-26 19:55:59 -0400

    Se realizaron todos los tasks requeridos.

--- src/main/java/org/practica/Main.java
+++ src/main/java/org/practica/Main.java
@@ -20,12 +20,48 @@ public class Main {
         System.out.println(url);
         Document doc_url = ConnectionUrl(url);
         System.out.println(doc_url.title());
-        Integer cantLines = CountLines(url);
-        Integer cantParagraphs = CountParagraphs(doc_url);
-        Integer cantImages = CountImages(doc_url);
-        Integer cantFormsByPost = CountForms(doc_url, "post");
-        Integer cantFormsByGet = CountForms(doc_url, "get");
-        ShowFormInputs(doc_url);
+        Integer selectedOption = 0;
+
+        do {
+            System.out.println("\nSeleccione una de las opciones o seleccione 0 si desea salir: \n 1-) Cantidad de líneas retornadas. \n 2-) Cantidad de párrafos (p).\n" +
+                    " 3-) Cantidad de imágenes. \n 4-) Cantidad de formularios por Metodos(POST y GET). \n 5-) Cada formulario mostrar los campos de input y su tipo." +
+                    "\n 6-) Identificar que el método de envío del formulario sea POST y enviar una petición al servidor. \n");
+            Scanner scan = new Scanner(System.in);
+            selectedOption = Integer.parseInt(scan.next());
+            switch (selectedOption){
+                case 1:
+                    Integer cantLines = CountLines(url);
+                    System.out.println("La cantidad es de: "+cantLines);
+                    break;
+                case 2:
+                    Integer cantParagraphs = CountParagraphs(doc_url);
+                    System.out.println("La cantidad es de: "+cantParagraphs);
+                    break;
+                case 3:
+                    Integer cantImages = CountImages(doc_url);
+                    System.out.println("La cantidad es de: "+cantImages);
+                    break;
+                case 4:
+                    Integer cantFormsByPost = CountForms(doc_url, "post");
+                    Integer cantFormsByGet = CountForms(doc_url, "get");
+                    System.out.println("La cantidad de forms por metodo POST: "+cantFormsByPost);
+                    System.out.println("La cantidad de forms por metodo GET: "+cantFormsByGet);
+                    break;
+                case 5:
+                    ShowFormInputs(doc_url);
+                    break;
+                case 6:
+                    FormParamsHeader(doc_url, "20170465");
+                    break;
+
+                default:
+                    System.out.println("\n");
+            }
+        }while (selectedOption != 0);
+
+
+
+
 
 
 
@@ -41,7 +77,7 @@ public class Main {
         String body = Jsoup.connect(url).execute().body();
         ///System.out.println(body);
         String[] bodyArr = body.split("\n");
-        System.out.println(bodyArr.length);
+        //System.out.println(bodyArr.length);
         return bodyArr.length;
 
     }
@@ -54,7 +90,7 @@ public class Main {
             //aumentar
             cantParagraphs++;
         }
-        System.out.println(cantParagraphs);
+        //System.out.println(cantParagraphs);
         return cantParagraphs;
     }
 
@@ -63,7 +99,7 @@ public class Main {
         Integer cantImages = 0;
         Elements images = document.select("p img");
         cantImages = images.size();
-        System.out.println(cantImages);
+        //System.out.println(cantImages);
         return cantImages;
     }
 
@@ -73,8 +109,8 @@ public class Main {
         Elements formElements = new Elements();
         formElements = document.select("form[method="+type.toLowerCase()+"]");
         cantFormsByMethod = formElements.size();
-        System.out.println(cantFormsByMethod);
-        System.out.println(formElements);
+        //System.out.println(cantFormsByMethod);
+        //System.out.println(formElements);
         return cantFormsByMethod;
 
     }
@@ -87,12 +123,26 @@ public class Main {
         for (Element input : inputsForms) {
             cantInputForm++;
             inputType.put("Input-"+cantInputForm, input.attr("type"));
+            System.out.println("Input-"+cantInputForm+ " "+ inputType.get("Input-"+cantInputForm));
         }
-        System.out.println(cantInputForm +"  "+inputType);
+        //System.out.println(cantInputForm +"  "+inputType);
     }
 
     //Realizar el envio de datos para un form con metodo post y enviar header
-    public static void FormParamsHeader(String url){
+    public static void FormParamsHeader(Document document, String matricula) throws IOException {
+        Elements postForms = document.select("form[method=post]");
+        Map<String, Document> formsSave = new HashMap<>();
+        Integer countForms = 0;
+        //Verificar el form obtenido
+        for (Element formSelected : postForms) {
+            countForms++;
+            String abosluteUrl = formSelected.absUrl("action");
+            Document documentResponse = Jsoup.connect(abosluteUrl).data("asignatura", "practica-1").header("matricula", matricula).post();
+            formsSave.put("Form-"+countForms, documentResponse);
+            System.out.println(formsSave.get("Form-"+countForms).body());
+        }
+
+
 
     }
 
