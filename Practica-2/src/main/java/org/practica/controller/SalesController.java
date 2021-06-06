a4ce2b29c6384c76ab14fe19dcd2a4ad2bf6a362
AliciaC07 <20170465@ce.pucmm.edu.do>
2021-06-06 14:45:48 -0400

    Javascript validation fixed.

--- Practica-2/src/main/java/org/practica/controller/SalesController.java
+++ Practica-2/src/main/java/org/practica/controller/SalesController.java
@@ -24,6 +24,11 @@ public class SalesController {
                     Map<String, Object> model = new HashMap<>();
                     model.put("title", "Sales");
                     model.put("class", "nav-link active");
+                    if (ctx.cookie("userName") != null || ctx.sessionAttribute("user") != null){
+                        model.put("isLogged", true);
+                    }else {
+                        model.put("isLogged", false);
+                    }
                     model.put("receipts", shop.getAllReceipts());
                     for (Receipt aux: shop.getAllReceipts()) {
                         System.out.println(aux.getClient().getName());;
